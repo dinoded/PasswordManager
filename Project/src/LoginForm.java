fca037815ed1bf474329316c8a0c2a24c09dc8c3
Sofiia Durytska <sofiiadurytska@gmail.com>
2025-03-09 20:13:34 +0300

    Update LoginForm.java

--- Project/src/LoginForm.java
+++ Project/src/LoginForm.java
@@ -63,8 +63,8 @@ public class LoginForm {
                 frame.setContentPane(form.getPanel1());
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 frame.pack();
-                frame.setSize(800, 600); // Устанавливаем размер формы
-                frame.setLocationRelativeTo(null); // Центрируем форму на экране
+                frame.setSize(800, 600);
+                frame.setLocationRelativeTo(null);
                 frame.setVisible(true);
             });
 
